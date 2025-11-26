f68caa4a89444aa02f80ba515661b5e7d95210eb
Alsycode <120390865+Alsycode@users.noreply.github.com>
2025-11-26 13:26:15 +0530

    lms

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -18,28 +18,33 @@ const mywork_data = [
         w_name:"MERN Learning Management System",
         w_img:project7_img
     },
+      {
+        w_no:3,
+        w_name:"Ecommerce App",
+        w_img:project8_img
+    },
     {
-        w_no:2,
+        w_no:4,
         w_name:"Service Connect",
         w_img:project2_img
     },
     {
-        w_no:3,
+        w_no:5,
         w_name:"Dating App",
         w_img:project3_img
     },
     {
-        w_no:4,
+        w_no:6,
         w_name:"Next Blog App",
         w_img:project4_img
     },
     {
-        w_no:5,
+        w_no:7,
         w_name:"NFT Preview card",
         w_img:project5_img
     },
     {
-        w_no:6,
+        w_no:8,
         w_name:"Restaurant Web app UI",
         w_img:project6_img
     },
